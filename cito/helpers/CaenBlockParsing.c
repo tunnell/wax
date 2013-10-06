a5884a1155cc06ff255fbe613e96af2c7f0c4ec6
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2013-10-06 16:13:59 +0200

    debugging

--- cito/helpers/CaenBlockParsing.c
+++ cito/helpers/CaenBlockParsing.c
@@ -141,7 +141,7 @@ int inplace(unsigned int *buff, int n)
 
 
 	Size=buff[pnt];              // size of next waveform
-	printf("size! %u", Size);
+	printf("size! %u\n", Size);
 	//if (CurrentChannel!=channel) { pnt+=Size; continue; }
 	//else pnt++;
 	//if (j>channel) return 0;      
