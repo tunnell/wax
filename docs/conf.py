1cbab01f9b305f5b49fd95f079a1c0ef6931c6be
tunnell <ctunnell@nikhef.nl>
2014-02-10 15:53:53 +0100

    Mock away in docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,33 @@ try:
 except ImportError:
     print("no ipython")
 
+
+#MOCK
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate',
+                'scipy.signal', 'numpy.dtype', 'scipy.signal._peak_finding', 'scipy.lib',
+                'scipy.lib.six', 'scipy.lib.six.moves', 'scipy.signal.wavelets', 'scipy.stats']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
