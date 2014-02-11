f6857e89b6b8cd056ae7bffdf31a540d81f54814
tunnell <ctunnell@nikhef.nl>
2014-02-11 23:36:39 +0100

    Add first file of test suite 'redux'

--- tests/TestWaveform.py
+++ tests/TestWaveform.py
@@ -1,11 +1,14 @@
 __author__ = 'tunnell'
 
 import unittest
+from cito.core import Waveform
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class WaveformTestCase(unittest.TestCase):
+    def test_empty_value_error(self):
+        with self.assertRaises(ValueError):
+            Waveform.get_samples(b'')
+
 
 
 if __name__ == '__main__':
