7b688afa9962f139ac8c88fefb19b15f9c837786
tunnell <ctunnell@nikhef.nl>
2014-02-12 14:27:39 +0100

    Bump version: 1.1.2 → 1.1.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ required = open('requirements.txt').read().splitlines()
 
 setup(
     name='cito',
-    version='1.1.2',
+    version='1.1.3',
     description='Generic particle-physics software trigger with flash ADCs.',
     long_description=readme + '\n\n' + history,
     author='Christopher Tunnell',
