fddf31a2f48a9378cdbaf46e07e5c49555523c58
tunnell <ctunnell@nikhef.nl>
2014-02-17 18:44:31 +0100

    Pull smoothing code out into own function

--- cito/Trigger/Threshold.py
+++ cito/Trigger/Threshold.py
@@ -8,68 +8,68 @@ import numpy as np
 from scipy.signal._peak_finding import _filter_ridge_lines, _identify_ridge_lines
 from scipy.signal.wavelets import ricker
 
+from itertools import groupby
+
+CWT_WIDTH = 150
+
+def subranges(indices):
+    """Identify groups of continuous numbers in a list
+
+    For example, if indices is:
+
+        [2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 20]
+
+    Then the location within the list of continuous ranges are at:
+
+        [(0,3), (4, 9), (10, 10)]
+
+    """
+    ranges = []
+    for k, g in groupby(enumerate(indices), lambda i_x:i_x[0]-i_x[1]):
+        # Each value is formated as: (location in array, value in array)
+        values = list(g)
+
+        # values[0] and values[-1] are range boundaries
+        ranges.append([values[0][0], values[-1][0]])
+
+    return ranges
+
 
 def trigger(indices, samples):
-    peaks = []
+    """Find peaks within contigous ranges
 
-    if len(indices) == len(samples) == 0:
-        return []
+    Within continous subranges, find peaks above threshold.
+    """
 
-    i_start = 0
-    index_last = None
-    for i, index in enumerate(indices):
-        if index_last is None or (index - index_last) == 1:
-            index_last = index_last
-        elif index <= index_last:
-            raise ValueError(
-                "Indices must be monotonically increasing: %d, %d!",
-                index,
-                index_last)
-        elif index - index_last > 1:
-            high_extrema = find_peaks(samples[i_start:i - 1])
-            for value in high_extrema:
-                peaks.append(value)
-            i_start = i
+    cwt_width=50 # samples
+
+    peaks = []  # Store the indices of peaks
+
+    ranges = subranges(indices)
+
+    combined_ranges = []
+
+    for subrange in ranges:
+        if len(combined_ranges) == 0:
+            combined_ranges.append(subrange)
+        elif combined_ranges[-1][1] + 2 * cwt_width > subrange[0]:
+            combined_ranges[-1][1] = subrange[1]
         else:
-            raise RuntimeError()
+            combined_ranges.append(subrange)
 
-    if i_start < (len(indices) - 1):  # If events still to process
-        high_extrema = find_peaks(samples[i_start:-1])
+    logging.debug("Ranges: %s" % str(ranges))
+
+    for subrange in combined_ranges:
+        subsamples = samples[subrange[0]:subrange[1]]
+
+        high_extrema = find_peaks(subsamples, cwt_width=cwt_width)
         for value in high_extrema:
             peaks.append(value)
 
     return peaks
 
 
-def cwt(data, wavelet, widths):
-    """Copied from SciPy; don't test"""
-    output = np.zeros([len(widths), len(data)])
-    for ind, width in enumerate(widths):
-        wavelet_data = wavelet(min(10 * width, len(data)), width)
-        output[ind, :] = signal.fftconvolve(data, wavelet_data,
-                                            mode='same')
-    return output
-
-
-def find_peaks_cwt(vector, widths, wavelet=None, max_distances=None, gap_thresh=None,
-                   min_length=None, min_snr=1, noise_perc=10):
-    """Copied from SciPy; don't test"""
-    if gap_thresh is None:
-        gap_thresh = np.ceil(widths[0])
-    if max_distances is None:
-        max_distances = widths / 4.0
-    if wavelet is None:
-        wavelet = ricker
-
-    cwt_dat = cwt(vector, wavelet, widths)
-    ridge_lines = _identify_ridge_lines(cwt_dat, max_distances, gap_thresh)
-    filtered = _filter_ridge_lines(cwt_dat, ridge_lines, min_length=min_length,
-                                   min_snr=min_snr, noise_perc=noise_perc)
-    max_locs = [x[1][0] for x in filtered]
-    return sorted(max_locs)
-
-
-def find_peaks(values, threshold=200, cwt_width=100):
+def find_peaks(values, threshold=1, cwt_width=CWT_WIDTH):
     """Find peaks within list of values.
 
     Uses scipy to find peaks above a threshold.
@@ -80,7 +80,7 @@ def find_peaks(values, threshold=200, cwt_width=100):
         cwt_width (float): The width of the wavelet that is convolved
 
     Returns:
-       np.array: Array of peak indecies
+       np.array: Array of peak indices
 
     """
 
@@ -88,8 +88,51 @@ def find_peaks(values, threshold=200, cwt_width=100):
     logging.debug('CWT with n=%d' % values.size)
     t0 = time.time()
     peakind = find_peaks_cwt(values, np.array([cwt_width]))
-    peaks_over_threshold = peakind  # [x for x in peakind if values[x] > threshold]
+    peaks_over_threshold = [x for x in peakind if values[x] > threshold]
     t1 = time.time()
 
     logging.debug('Convolution duration: %f s' % (t1 - t0))
     return np.array(peaks_over_threshold, dtype=np.uint32)
+
+
+def smooth(vector, widths=np.array([CWT_WIDTH])):
+    width = widths[0]
+    cwt_dat = np.zeros([1, len(vector)])
+    wavelet = ricker
+    wavelet_data = wavelet(min(10 * width, len(vector)), width)
+    cwt_dat[0, :] = signal.fftconvolve(vector, wavelet_data,
+                                       mode='same')
+    return cwt_dat
+
+
+def find_peaks_cwt(vector, widths=np.array([CWT_WIDTH])):
+    """Copied from SciPy; don't test"""
+    gap_thresh = np.ceil(widths[0])
+    max_distances = widths / 4.0
+
+    cwt_dat = smooth(vector, widths)
+
+    ridge_lines = _identify_ridge_lines(cwt_dat, max_distances, gap_thresh)
+    filtered = _filter_ridge_lines(cwt_dat, ridge_lines, min_length=None,
+                                   min_snr=1, noise_perc=10)
+    max_locs = [x[1][0] for x in filtered]
+
+
+        ## the next code is just to visualize how this works
+    import matplotlib.pyplot as plt
+    plt.plot(vector)  # extent=(widths[0], widths[-1], times[0], times[-1]))
+    plt.plot(cwt_dat[0])
+    for l in ridge_lines:
+        plt.plot(l[1], l[0], 'k-')
+    for l in filtered:
+        plt.plot(l[1], l[0], 'r-')
+    #plt.plot(peaks_t, peaks_w, 'k*')  # not working
+    plt.show()
+
+    return sorted(max_locs)
+
+
+
+
+
+
--- tests/TestThreshold.py
+++ tests/TestThreshold.py
@@ -1,1 +1,14 @@
 __author__ = 'tunnell'
+
+from cito.Trigger import Threshold
+import unittest
+
+class ThresholdTestCase(unittest.TestCase):
+    def test_subrange(self):
+        self.assertEqual(Threshold.subranges([2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 20]),
+                         [[0,3], [4, 9], [10, 10]])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
