c66912e5f362b7a11044ba2c711fe3169dce1e00
tunnell <ctunnell@nikhef.nl>
2014-03-19 19:24:10 +0100

    Bump version: 1.8.8 → 1.9.0

--- cito/__init__.py
+++ cito/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'tunnell'
-__version__ = '1.8.8'
+__version__ = '1.9.0'
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ required = open('requirements.txt').read().splitlines()
 
 setup(
     name='cito',
-    version='1.8.8',
+    version='1.9.0',
     description='Generic particle-physics software trigger with flash ADCs.',
     long_description=readme + '\n\n' + history,
     author='Christopher Tunnell',
