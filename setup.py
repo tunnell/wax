9009fd98417a3f2ce49b0aeb4e1fe77361dbb83d
Christopher Tunnell <ctunnell@nikhef.nl>
2014-04-24 16:27:13 +0200

    Bump version: 2.1.1 → 2.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ required = open('requirements.txt').read().splitlines()
 
 setup(
     name='wax',
-    version='2.1.1',
+    version='2.1.2',
     description='Generic particle-physics software trigger and data processor.',
     long_description=readme + '\n\n' + history,
     author='Christopher Tunnell',
