9e6c58049c0572e17fd2718f030c9df32f8520b3
Christopher Tunnell <ctunnell@nikhef.nl>
2014-10-08 18:08:58 +0200

    Compute processing time to be one second before latest doc

--- wax/EventBuilder/ebcore.cpp
+++ wax/EventBuilder/ebcore.cpp
@@ -193,8 +193,7 @@ u_int32_t ProcessTimeRangeTask(int64_t t0, int64_t t1,
 
        cout<<"output_docs.size() "<<output_docs.size()<<endl;
     //conn.setWriteConcern(WriteConcern::unacknowledged);
-    conn.insert(mongo_output_location,
-                output_docs);
+    conn.insert(mongo_output_location,  output_docs);
 
     cerr<<"processed_size"<<processed_size<<" triggered_size "<<triggered_size<<endl;
     return processed_size;
